7024aaa74d60745ce1287e82470bcb02fb51761f
casliann <dilaraamina2005@gmail.com>
2025-04-07 20:36:25 +0200

    test2

--- build/CMakeFiles/bank.dir/src/account.c.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/bank.dir/src/account.c.o: /opt/bank-dimchov_b/src/account.c \
- /usr/include/stdc-predef.h
--- build/CMakeFiles/bank.dir/src/user.c.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/bank.dir/src/user.c.o: /opt/bank-dimchov_b/src/user.c \
- /usr/include/stdc-predef.h
