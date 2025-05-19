45324df6aceed6628d1b864d76efe6c3a19ab959
dandadan00 <iza.tanpi@gmail.com>
2025-05-19 18:29:06 +0200

    Add gcov to protocol and yml

--- include/account.h
+++ include/account.h
@@ -1,13 +1,13 @@
 #ifndef ACCOUNT_H
 #define ACCOUNT_H
 
-#include "customer.h"
+#include "types.h"
 
-int login(User *user);
 int create_account(User *user);
+int login(User *user);
+int get_int_value();
 int delete_account(User *user);
 int check_customer_balance(User *user);
 int check_number_of_accounts(const char *name, const char *ssn);
-int get_int_value();
 
 #endif
