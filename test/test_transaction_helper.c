c648dfa20659d2b97fdfbb4dc49b0b03a4ea88b7
dandadan00 <iza.tanpi@gmail.com>
2025-05-26 03:01:35 +0200

    Unit test refactored transaction_helper

--- test/test_transaction_helper.c
+++ test/test_transaction_helper.c
@@ -15,82 +15,109 @@ void teardown(void) {
 }
 
 // ======= UNIT TEST =======
-// ======= 01 deposit()
-// ======= 02 withdraw()
+// ======= 00 mock dependecies
+// ======= 01 dependecies
+// ======= 02 deposit()
+// ======= 03 withdraw()
 
 // =========================
-// ======= 01 deposit()
-// ======= core logic:
-//         validate_amount_to_deposit(const char *input)
-// ======= other dependencies:
-//         clear_buffer()
-//         amount_to_deposit()
-// =========================
-
-// ======= validate_amount_to_deposit(const char *input)
-void test_validate_amount_to_deposit_VALID(void) {
-    double result = validate_amount_to_deposit("15.15");
-    TEST_ASSERT_EQUAL_DOUBLE(15.15, result);
-    // TEST_ASSERT_DOUBLE_WITHIN(0.0001, 15.15, result);
-}
-
-void test_validate_amount_to_deposit_NEGATIVE(void) {
-    double result = validate_amount_to_deposit("-15.15");
-    TEST_ASSERT_EQUAL_DOUBLE(0.0, result);
-}
-
-void test_validate_amount_to_deposit_INVALID_STRING(void) {
-    double result = validate_amount_to_deposit("not_a_number");
-    TEST_ASSERT_EQUAL_DOUBLE(0.0, result);
-}
-
-void test_validate_amount_to_deposit_EMPTY_INPUT(void) {
-  double result = validate_amount_to_deposit("");
-    TEST_ASSERT_EQUAL_DOUBLE(0.0, result);
-}
-
-void test_validate_amount_to_deposit_MIXED_DATATYPES_LEADING_NUM(void) {
-  double result = validate_amount_to_deposit("15.15AbCdEfg");
-  TEST_ASSERT_EQUAL_DOUBLE(15.15, result);
-  // TEST_ASSERT_EQUAL_DOUBLE(0.0, result);
-}
-
-void test_validate_amount_to_deposit_MIXED_DATATYPES_LEADING_CHAR(void) {
-    double result = validate_amount_to_deposit("a15.15AbCdEfg");
-    TEST_ASSERT_EQUAL_DOUBLE(0.0, result);
-}
-
-void test_validate_amount_to_deposit_WHITESPACE(void) {
-  double result = validate_amount_to_deposit("    15.15 ");
-  TEST_ASSERT_EQUAL_DOUBLE(15.15, result);
-  //TEST_ASSERT_DOUBLE_WITHIN(0.0001, 15.15, result);
-}
-
-// ======= clear_buffer()
-void test_clear_buffer(void) {
-  TEST_ASSERT_EQUAL(0, clear_buffer());
-  // TEST_IGNORE();
-}
-
-// ======= amount_to_deposit()
+// ======= 00 mock dependecies
 void mock_stdin(const char *input) {
-    FILE *file = fopen("test/mock_input.txt", "w+");
+    FILE *file = fopen("test/mock_input.txt", "w");
     fprintf(file, "%s\n", input);
-    rewind(file);
+    fclose(file);
+
+    // rewind(file);
     freopen("test/mock_input.txt", "r", stdin);
 }
 
-void test_amount_to_deposit_VALID(void) {
-    clear_buffer();
-  mock_stdin("15.15");
-  double result = amount_to_deposit();
-  TEST_ASSERT_EQUAL_DOUBLE(15.15, result);
-  //TEST_IGNORE();
+// =========================
+// ======= 01 dependencies:
+//         clear_buffer()
+//         parse_amount()
+//         validate_amount_for_transaction()
+//         amount_to_deposit()
+// =========================
+
+// ======= clear_buffer()
+void test_clear_buffer(void) {
+    TEST_ASSERT_EQUAL(0, clear_buffer());
+    // TEST_IGNORE();
+}
+
+// ======= parse_amount()
+void test_parse_amount_VALID(void) {
+    TEST_ASSERT_EQUAL_DOUBLE(15.15, parse_amount("15.15"));
+}
+
+void test_parse_amount_INVALID_STRING(void) {
+    TEST_ASSERT_EQUAL_DOUBLE(0.0, parse_amount("qwerty"));
+}
+
+void test_parse_amount_INVALID_STRING_TRAIL(void) {
+    TEST_ASSERT_EQUAL_DOUBLE(0.0, parse_amount("123qwerty"));
+}
+
+void test_parse_amount_INVALID_STRING_LEAD(void) {
+    TEST_ASSERT_EQUAL_DOUBLE(0.0, parse_amount("as123"));
+}
+
+// ======= validate_amount_for_transaction()
+
+void test_validate_amount_for_transaction_VALID(void) {
+    mock_stdin("15.15");
+    double result = validate_amount_for_transaction("deposit");
+    TEST_ASSERT_EQUAL_DOUBLE(15.15, result);
+}
+
+void test_validate_amount_for_transaction_VALID_WITH_WHITESPACE(void) {
+    mock_stdin(" 15.15  ");
+    double result = validate_amount_for_transaction("withdraw");
+    TEST_ASSERT_EQUAL_DOUBLE(15.15, result);
+}
+
+void test_validate_amount_for_transaction_INVALID_NEGATIVE_NUMBER(void) {
+    mock_stdin("-15.15");
+    double result = validate_amount_for_transaction("withdraw");
+    TEST_ASSERT_EQUAL_DOUBLE(0.0, result);
+}
+
+void test_validate_amount_for_transaction_INVALID_STRING(void) {
+    mock_stdin("qwerty");
+    double result = validate_amount_for_transaction("withdraw");
+    TEST_ASSERT_EQUAL_DOUBLE(0.0, result);
+}
+
+void test_validate_amount_for_transaction_INVALID_STRING_TRAIL(void) {
+    mock_stdin("15.15qwerty");
+    double result = validate_amount_for_transaction("deposit");
+    TEST_ASSERT_EQUAL_DOUBLE(0.0, result);
+}
+
+void test_validate_amount_for_transaction_INVALID_STRING_TRAIL_WITH_WHITESPACE(void) {
+    mock_stdin("15.15qwerty    ");
+    double result = validate_amount_for_transaction("withdraw");
+    TEST_ASSERT_EQUAL_DOUBLE(0.0, result);
+}
+
+void test_validate_amount_for_transaction_INVALID_STRING_LEAD(void) {
+    mock_stdin("a15.15qwerty");
+    double result = validate_amount_for_transaction("withdraw");
+    TEST_ASSERT_EQUAL_DOUBLE(0.0, result);
+}
+
+void test_validate_amount_for_transaction_INVALID_STRING_WITH_WHITESPACE(void) {
+    mock_stdin(" 15.15 qwerty");
+    double result = validate_amount_for_transaction("withdraw");
+    TEST_ASSERT_EQUAL_DOUBLE(0.0, result);
+}
+
+// ======= validate_transaction()
+void test_validate_transaction_VALID(void) {
+
 }
 
 // =========================
-// ======= 02 withdraw()
-// ======= core logic:
-//         withdraw_from_user()
-//         validate_amount_to_deposit()
+// ======= 02 deposit:
+//
 // =========================
