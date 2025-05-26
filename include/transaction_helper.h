94fcc4a8136012ba699e6ef6056a7f0e49795cf8
dandadan00 <iza.tanpi@gmail.com>
2025-05-26 02:14:22 +0200

    Optimized deposit()

--- include/transaction_helper.h
+++ include/transaction_helper.h
@@ -3,11 +3,14 @@
 
 #include "types.h"
 
-double validate_amount_to_deposit(const char *input);
 int clear_buffer();
+double parse_amount(const char *input);
+double validate_amount_for_transaction(const char *action);
+double validate_transaction(User *user, const char *action);
+
+// double validate_amount_to_deposit(const char *input);
 double amount_to_deposit();
 
-double validate_transaction(User *user, const char *action);
 double amount_to_withdraw(User *user);
 int withdraw_from_user(User *user, double amount);
 
