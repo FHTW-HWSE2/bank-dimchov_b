208f41e90ca800d487f4f22757b4a617d02b2cc1
dandadan00 <iza.tanpi@gmail.com>
2025-05-26 17:33:46 +0200

    Removed input flush in transfer()

--- src/transaction.c
+++ src/transaction.c
@@ -2,6 +2,7 @@
 #include "../include/transaction.h"
 #include "../include/account.h"
 #include "../include/customer.h"
+#include "../include/types.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
