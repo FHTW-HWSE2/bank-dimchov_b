ee96b89fd147d2018c44acd9d9b190f318e86daa
dandadan00 <iza.tanpi@gmail.com>
2025-05-26 02:07:03 +0200

    Refactor user input stream for better testing, add test/build directory to gitignore

--- test/test_transaction_helper.c
+++ test/test_transaction_helper.c
@@ -3,9 +3,16 @@
 #include "mock_customer.h"
 #include "types.h"
 #include "transaction_helper.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 void setup(void) {}
-void teardown(void) {}
+void teardown(void) {
+    remove("test/mock_input.txt");
+    freopen("CON", "r", stdin);
+    // freopen("/dev/tty", "r", stdin); // Linux, Mac
+}
 
 // ======= UNIT TEST =======
 // ======= 01 deposit()
@@ -14,10 +21,13 @@ void teardown(void) {}
 // =========================
 // ======= 01 deposit()
 // ======= core logic:
-//         validate_amount_to_deposit()
+//         validate_amount_to_deposit(const char *input)
 // ======= other dependencies:
 //         clear_buffer()
+//         amount_to_deposit()
 // =========================
+
+// ======= validate_amount_to_deposit(const char *input)
 void test_validate_amount_to_deposit_VALID(void) {
     double result = validate_amount_to_deposit("15.15");
     TEST_ASSERT_EQUAL_DOUBLE(15.15, result);
@@ -56,14 +66,26 @@ void test_validate_amount_to_deposit_WHITESPACE(void) {
   //TEST_ASSERT_DOUBLE_WITHIN(0.0001, 15.15, result);
 }
 
+// ======= clear_buffer()
 void test_clear_buffer(void) {
   TEST_ASSERT_EQUAL(0, clear_buffer());
   // TEST_IGNORE();
 }
 
-void test_amount_to_deposit(void) {
-  // TEST_ASSERT_EQUAL_DOUBLE(15.15, amount_to_deposit());
-  TEST_IGNORE();
+// ======= amount_to_deposit()
+void mock_stdin(const char *input) {
+    FILE *file = fopen("test/mock_input.txt", "w+");
+    fprintf(file, "%s\n", input);
+    rewind(file);
+    freopen("test/mock_input.txt", "r", stdin);
+}
+
+void test_amount_to_deposit_VALID(void) {
+    clear_buffer();
+  mock_stdin("15.15");
+  double result = amount_to_deposit();
+  TEST_ASSERT_EQUAL_DOUBLE(15.15, result);
+  //TEST_IGNORE();
 }
 
 // =========================
