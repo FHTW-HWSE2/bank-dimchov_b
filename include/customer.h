7eac4f857377d5d9a9c685567589d34b76e60b33
dandadan00 <iza.tanpi@gmail.com>
2025-06-16 07:02:01 +0200

    Refactor files for testing, unit test deposit(), edited/added protocols
    # Conflicts:
    #	include/customer.h
    #	include/transaction.h
    #	src/main.c
    #	src/transaction.c

--- include/customer.h
+++ include/customer.h
@@ -20,6 +20,7 @@ typedef struct {
 
 void report(User *user);
 void generate_transaction_report(); 
+// void simulate_7_days(User *users, int user_count);
 void save_account_to_csv(User *user, char *filename);
 int update_balance_in_csv(User *user, double new_amount);
 void execute_hardcoded_transactions(User *users, int user_count);
--- include/transaction.h
+++ include/transaction.h
@@ -11,10 +11,12 @@ typedef struct {
     double amount;
 } Transaction;
 
-double deposit();
+int deposit(User *users);
+//double amount_to_deposit();
 double withdraw(User *user);
 double amount_to_transfer(User *user);
 int transfer(User *user, double amount);
+// void simulate_7_days(User *users, int user_count);
 int transfer_simulation();
 
 #endif 
